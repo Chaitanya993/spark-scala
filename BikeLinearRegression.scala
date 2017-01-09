1277cfdac7f4546f5501d871fcd98d42edb1c20d
Chaitanya993 <chaitanyakondapalli11@gmail.com>
2017-01-09 15:26:46 -0600

    Add files via upload

--- BikeLinearRegression.scala
+++ BikeLinearRegression.scala
@@ -48,86 +48,86 @@ object BikeLinearRegression {
 
     import spark.implicits._
     //Read Training data
-    /*val data = loadTrainingData(spark)
+    val data = loadTrainingData(spark)
 
     //build pipeline model
     val ppl = LRPipeline
 
     //test model with training data
-    val model = ppl.fit(data)*/
+    val model = ppl.fit(data)
 
     val test = spark.read.format("csv").option("header", "true").load("data/sales.txt")
 
     test.createOrReplaceTempView("temtbl")
 
     val testsql = spark.sql("select a,d from temtbl").show()
-    //   /* val trainingPredict = model.transform(data)
-    //      .withColumnRenamed("prediction","Predictcnt")
-    //      .withColumnRenamed("label","Inputcnt")
-    //      .withColumnRenamed("instant","instant")
-    //      //.select("instant", "Predictcnt","Inputcnt")
-    //    trainingPredict.show()
-    //
-    //    //Predict using test date
-    //    val dfPredict = loadTestgData(spark)
-    //    val predict = model.transform(dfPredict)
-    //      .withColumnRenamed("prediction","Predictcnt")
-    //      .withColumnRenamed("label","Inputcnt")
-    //      .withColumnRenamed("instant","instant")
-    //      .select("instant", "Predictcnt","Inputcnt")
-    //
-    //    println("==================Started ========================")
-    //    predict.show()
-    //    println("==================End=============================")
-    //
-    //    val regparams = new RegressionMetrics(predict.map(_.mkString(",")).map(_.split(",")).rdd
-    //                                            .map(a => (a(0).toDouble,a(1)toDouble)))
-    //    println("================" + regparams.r2 + "===================")
-    //  }
-    //
-    //
-    //  def LRPipeline():Pipeline = {
-    //    val lr = new LinearRegression()
-    //    lr.setMaxIter(1000)
-    //    lr.setRegParam(0.01)
-    //    val assembler = new VectorAssembler().setInputCols(
-    //      Array("weekday","workingday","holiday","weathersit",
-    //        "temp","atemp","hum","windspeed"))
-    //      .setOutputCol("features")
-    //    val pipeline = new Pipeline().setStages(Array(assembler,lr))
-    //    return pipeline
-    //  }
-    //
-    //
-    //
-    //  def loadTrainingData(Sparksession:SparkSession):DataFrame = {
-    //    val df = Sparksession.read.json("data/bike.json").na.drop()
-    //    df.registerTempTable("bikeDataTbl")
-    //    Sparksession.sql("select instant," +
-    //      "cast (dteday as date) dteday, " +
-    //      "cast (season as int) season, cast(yr as int) yr, " +
-    //      "cast(mnth as int) mnth, cast (hr as int) hr, " +
-    //      "cast (holiday as int) holiday, cast (weekday as int) weekday, " +
-    //      "cast (workingday as int) workingday,    " +
-    //      "cast (weathersit as int) weathersit, " +
-    //      "cast (temp as double) temp ,cast (atemp as double) atemp, " +
-    //      "cast (hum as double) hum, cast (windspeed as int) windspeed,        " +
-    //      "cast (casual as int) casual, cast (registered as int) registered, " +
-    //      "cast (cnt as Double) label from bikeDataTbl")
-    //  }
-    //
-    //  def loadTestgData(Sparksession:SparkSession):DataFrame = {
-    //    val df = Sparksession.read.json("data/bike Test.json").na.drop()
-    //    df.registerTempTable("bikeDataTbl")
-    //    Sparksession.sql("select instant,cast (dteday as date) dteday, " +
-    //      "cast (season as int) season, cast(yr as int) yr, " +
-    //      "cast(mnth as int) mnth, cast (hr as int) hr, cast (holiday as int) holiday, " +
-    //      "cast (weekday as int) weekday, cast (workingday as int) workingday,    " +
-    //      "cast (weathersit as int) weathersit, cast (temp as double) temp ," +
-    //      "cast (atemp as double) atemp, cast (hum as double) hum, " +
-    //      "cast (windspeed as int) windspeed,        " +
-    //      "cast (casual as int) casual, cast (registered as int) registered, " +
-    //      "cast (cnt as Double)  label from bikeDataTbl")
+    val trainingPredict = model.transform(data)
+       .withColumnRenamed("prediction","Predictcnt")
+       .withColumnRenamed("label","Inputcnt")
+       .withColumnRenamed("instant","instant")
+       //.select("instant", "Predictcnt","Inputcnt")
+     trainingPredict.show()
+
+     //Predict using test date
+     val dfPredict = loadTestgData(spark)
+     val predict = model.transform(dfPredict)
+       .withColumnRenamed("prediction","Predictcnt")
+       .withColumnRenamed("label","Inputcnt")
+       .withColumnRenamed("instant","instant")
+       .select("instant", "Predictcnt","Inputcnt")
+
+     println("==================Started ========================")
+     predict.show()
+     println("==================End=============================")
+
+     val regparams = new RegressionMetrics(predict.map(_.mkString(",")).map(_.split(",")).rdd
+                                             .map(a => (a(0).toDouble,a(1)toDouble)))
+     println("================" + regparams.r2 + "===================")
+   }
+
+
+   def LRPipeline():Pipeline = {
+     val lr = new LinearRegression()
+     lr.setMaxIter(1000)
+     lr.setRegParam(0.01)
+     val assembler = new VectorAssembler().setInputCols(
+       Array("weekday","workingday","holiday","weathersit",
+         "temp","atemp","hum","windspeed"))
+       .setOutputCol("features")
+     val pipeline = new Pipeline().setStages(Array(assembler,lr))
+     return pipeline
+   }
+
+
+
+   def loadTrainingData(Sparksession:SparkSession):DataFrame = {
+     val df = Sparksession.read.json("data/bike.json").na.drop()
+     df.registerTempTable("bikeDataTbl")
+     Sparksession.sql("select instant," +
+       "cast (dteday as date) dteday, " +
+       "cast (season as int) season, cast(yr as int) yr, " +
+       "cast(mnth as int) mnth, cast (hr as int) hr, " +
+       "cast (holiday as int) holiday, cast (weekday as int) weekday, " +
+       "cast (workingday as int) workingday,    " +
+       "cast (weathersit as int) weathersit, " +
+       "cast (temp as double) temp ,cast (atemp as double) atemp, " +
+       "cast (hum as double) hum, cast (windspeed as int) windspeed,        " +
+       "cast (casual as int) casual, cast (registered as int) registered, " +
+       "cast (cnt as Double) label from bikeDataTbl")
+   }
+
+   def loadTestgData(Sparksession:SparkSession):DataFrame = {
+     val df = Sparksession.read.json("data/bike Test.json").na.drop()
+     df.registerTempTable("bikeDataTbl")
+     Sparksession.sql("select instant,cast (dteday as date) dteday, " +
+       "cast (season as int) season, cast(yr as int) yr, " +
+       "cast(mnth as int) mnth, cast (hr as int) hr, cast (holiday as int) holiday, " +
+       "cast (weekday as int) weekday, cast (workingday as int) workingday,    " +
+       "cast (weathersit as int) weathersit, cast (temp as double) temp ," +
+       "cast (atemp as double) atemp, cast (hum as double) hum, " +
+       "cast (windspeed as int) windspeed,        " +
+       "cast (casual as int) casual, cast (registered as int) registered, " +
+       "cast (cnt as Double)  label from bikeDataTbl")
     //  }*/
 
 
